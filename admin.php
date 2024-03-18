ca95e6a1114fe766cc54c1a58527547d70b33fd8
MS-SHARADI <97178480+MS-SHARADI@users.noreply.github.com>
2024-03-18 13:31:15 +0530

    Add files via upload

---
+++ admin.php
@@ -0,0 +1,30 @@
+<html lang="en">
+<head>
+
+    <title>registration form</title>
+    <link rel="stylesheet" href="./css/register.css">
+</head>
+<body>
+    <div class="container">
+    <div class="title">ADMIN</div>
+    <form action="#">
+        <div class="user-details">
+            <div class="input-box">
+                <span class="details">Admin ID</span>
+                <input type="number" placeholder="Enter your Admin Id" required>
+            </div>
+           
+            <div class="input-box">
+                <span class="details">Password</span>
+                <input type="password" placeholder="Enter your password" required>
+            </div>
+            
+            
+        <div class="button">
+            <input type="submit" class="Login">
+        </div>
+    
+    </form>
+    </div>    
+</body>
+</html>
---
+++ voter.php
@@ -0,0 +1,30 @@
+<html lang="en">
+<head>
+
+    <title>voter login</title>
+    <link rel="stylesheet" href="./css/register.css">
+</head>
+<body>
+    <div class="container">
+    <div class="title">VOTER</div>
+    <form action="#">
+        <div class="user-details">
+            <div class="input-box">
+                <span class="details">Voter ID</span>
+                <input type="number" placeholder="Enter your Voter Id" required>
+            </div>
+           
+            <div class="input-box">
+                <span class="details">Password</span>
+                <input type="password" placeholder="Enter your password" required>
+            </div>
+            
+            
+        <div class="button">
+            <input type="submit" class="Login">
+        </div>
+    
+    </form>
+    </div>    
+</body>
+</html>
